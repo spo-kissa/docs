1597d33a4e8d2746d1e10b843719ec09b4b8b702
Ii Daisuke <dai@e-frontier.systems>
2025-10-17 10:19:46 +0900

    Midnight-Nodeの構築手順を加筆

--- dev.bat
+++ dev.bat
@@ -3,4 +3,6 @@
 rem docker build -t mkdocs:dev .
 rem docker run --rm -it -p 8000:8000 -v ./spo-docs:/docs mkdocs:dev
 
-docker run --rm -it -p 8000:8000 -v ./spo-docs:/docs squidfunk/mkdocs-material
+rem docker run --rm -it -p 8000:8000 -e WATCHDOG_FORCE_POLLING=true -v ./spo-docs:/docs squidfunk/mkdocs-material serve
+
+docker run --rm -it -p 8000:8000 -e WATCHDOG_FORCE_POLLING=true -v ./spo-docs:/docs squidfunk/mkdocs-material:latest
